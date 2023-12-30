284bba177aac4c13768cd630f71994bac8b8aba5
Burak Yüksel <buriii@hotmail.de>
2023-12-30 23:56:53 +0100

    add navbar elemets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,24 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <p>Navbar</p>;
+  return (
+    <div>
+      <ul className="navbar-ul-container">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Offer</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
